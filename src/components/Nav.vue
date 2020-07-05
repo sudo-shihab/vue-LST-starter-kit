fa5845f549832c64068032b620c728382c27f3d6
sudo-shihab <contact.shihab.me@gmail.com>
2020-07-05 05:45:04 +0530

    added tailwind config to storybook config file

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -62,20 +62,6 @@
 
 <script>
 export default {
-  name: "my-button",
-
-  methods: {},
+  name: "Nav",
 };
 </script>
-
-<style scoped>
-button {
-  border: 1px solid #eee;
-  border-radius: 3px;
-  background-color: #ffffff;
-  cursor: pointer;
-  font-size: 15pt;
-  padding: 3px 10px;
-  margin: 10px;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
+    <Nav class="mb-6" />
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" class="text-center" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Nav,
+  },
 };
 </script>
